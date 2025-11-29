5762e5cd372b7e3e9a201edf8b8a691a34ebf034
satyk093 <saty.k.0021@gmail.com>
2025-11-29 23:06:33 +0530

    add new file

---
+++ Simple_Program/SumTwoNumber.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+
+    printf("Enter First Number: ");
+    scanf("%d", &num1);
+
+    printf("Enter Second Number: ");
+    scanf("%d", &num2); 
+
+    sum = num1 + num2;
+
+    printf("SUM = %d\n", sum);
+
+    return 0;
+}
